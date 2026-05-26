2f348bef00b85dd8818f2a632d2a9034819bb777
jeejaw-star <jeejaw@gmail.com>
2026-05-26 18:02:10 -0400

    Added box for inputting time

--- config.gd
+++ config.gd
@@ -4,15 +4,18 @@ extends Control
 @onready var p2 = %p2_name
 @onready var p3 = %p3_name
 @onready var p4 = %p4_name
+@onready var time = %time
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	p1.text = "p1"
-	p2.text = "p2"
-	p3.text = "p3"
-	p4.text = "p4"
+	p1.text = "Player 1"
+	p2.text = "Player 2"
+	p3.text = "Player 3"
+	p4.text = "Player 4"
 	color.color = Color.from_hsv(randf(), 1.0, 1.0)
+	time.value = 300
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
